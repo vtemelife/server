6e104119d25bb654ca9abb0273405f9cc133cfdb
vsdudakov <vsdudakov@gmail.com>
2020-05-07 01:56:40 +0300

    Fixed .env, added tqdm for progress, fixed thumbnails management command

--- apps/storage/management/commands/generate_thumbnails.py
+++ apps/storage/management/commands/generate_thumbnails.py
@@ -1,12 +1,16 @@
 from django.core.management.base import BaseCommand
+from tqdm import tqdm
 
 from apps.storage.models import Image
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for image in Image.objects.filter(is_deleted=False):
-            image.thumbnail_100x100.generate()
-            image.thumbnail_500x500.generate()
-            image.thumbnail_blur_100x100.generate()
-            image.thumbnail_blur_500x500.generate()
+        for image in tqdm(Image.objects.filter(is_deleted=False)):
+            try:
+                image.thumbnail_100x100.generate()
+                image.thumbnail_500x500.generate()
+                image.thumbnail_blur_100x100.generate()
+                image.thumbnail_blur_500x500.generate()
+            except FileNotFoundError:
+                pass
